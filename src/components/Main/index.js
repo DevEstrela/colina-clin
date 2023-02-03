96cf0b3e3204d1e49f9bc3ec01614b8c8361c714
Henrique Estrela <henrique.estrela50@hotmail.com>
2023-02-03 15:43:25 -0300

    edit-mobile

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,12 +10,12 @@ function Main(){
           
                 <div className={styles.containerImg} 
                    data-aos="fade-down"
-                   data-aos-offset="250"      //deslocamento (em px) do ponto de disparo original
+                   data-aos-offset="100"      //deslocamento (em px) do ponto de disparo original
                    data-aos-delay="200"      //valores de 0 a 3000, com passo 50ms
                    data-aos-duration="500"  //atenuação padrão para animações AOS
                    data-aos-mirror="false"  //se a animação deve acontecer apenas uma vez - enquanto rola para baixo
                    data-aos-once="false"     //se os elementos devem ser animados ao passar por eles
-                   data-aos-anchor-placement="center"
+                   data-aos-anchor-placement=""
                    data-aos-easing="ease-in-sine"
                 >
                     <img
@@ -27,7 +27,17 @@ function Main(){
 
                 <div className={styles.containerMedicos}>
                     <article>
-                        <h1 className={styles.title}>Nós cuidamos de Você</h1>
+                        <h1 className={styles.title}
+                        data-aos="fade-left"
+                        data-aos-offset="50"      //deslocamento (em px) do ponto de disparo original
+                        data-aos-delay="200"      //valores de 0 a 3000, com passo 50ms
+                        data-aos-duration="500"  //atenuação padrão para animações AOS
+                        data-aos-mirror="false"  //se a animação deve acontecer apenas uma vez - enquanto rola para baixo
+                        data-aos-once="false"     //se os elementos devem ser animados ao passar por eles
+                        data-aos-anchor-placement=""
+                        data-aos-easing="ease-in-sine"
+                        
+                        >Nós cuidamos de Você</h1>
                         <p className={styles.text}>Nós da colinaclin vamos das todo suporte e o<strong><span> melhor atendimento </span></strong> para você se sentir muito confortavel e ser bem atendido pela nossa equipe de doutores e especialista das area da saude. Contamos com os <strong><span> melhores equipamentos </span></strong>  que vai atender suas necessidades sem que você precise ir a outro lugar realizar algum tipo de exame extra.</p>
                     </article>
 
